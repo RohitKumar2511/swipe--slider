d7f0e963a80ae7d6e602d662ee3f263cad7393bd
RohitKumar2511 <55598328+RohitKumar2511@users.noreply.github.com>
2020-01-02 15:56:46 +0000

    Add files via upload

---
+++ function.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($)
+{
+    "use strict";
+    $("#testimonials-list").owlCarousel(
+        {
+            loop:true,
+            center: true,
+            items: 3,
+            margin: true,
+            autoplay: true,
+            dots: true,
+            autoplayTimeout: 1000,
+            smartSpeed: 11,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    item: 2
+                },
+                1170: {
+                    items: 3
+                }
+            }
+        });
+});
